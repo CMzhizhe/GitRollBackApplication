cad7f87d2909d54d5e9351b541569d08f94ff3ee
gaoxiaoxiong <745570899@qq.com>
2023-09-13 03:09:33 +0800

    init

---
+++ app/src/main/java/com/example/gitrollbackapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gitrollbackapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i(TAG,"假如这里是大量的代码============ 开始")
+
+        Log.i(TAG,"代码，代码，代码，代码，初始化版本第一代，init的代码")
+
+        Log.i(TAG,"假如这里是大量的代码============ 结束")
+    }
+}
